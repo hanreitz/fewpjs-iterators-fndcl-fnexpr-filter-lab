c31e9cd8a040effd74381ee606f7568664402312
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-04 22:46:18 -0400

    commit tree f19ee3f803d96e375bf935504b0dcf478e332858

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (drivers, search) {
+  const matches = drivers.filter(d => d.toLowerCase() === search.toLowerCase())
+  return !!matches ? matches : [];
+}
+
+function fuzzyMatch (drivers, str) {
+  const matches = drivers.filter (d => d.slice(0, str.length) === str)
+  return !!matches ? matches : [];
+}
+
+function matchName (drivers, search) {
+  const matches = drivers.filter (d => d.name === search);
+  return !!matches ? matches : [];
+}
